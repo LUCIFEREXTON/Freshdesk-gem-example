74cc764a0e80986d45421c9e0f68a79252ac82f7
LUCIFEREXTON <gupta13utkarsh@gmail.com>
2022-03-31 13:50:54 +0530

    App Version 1.2.0

--- config/initializers/freshdesk.rb
+++ config/initializers/freshdesk.rb
@@ -2,4 +2,4 @@ require 'freshdesk'
 
 # Freshdesk.config <Your Freshdesk Api key>, <Your freshdesk baseurl>
 # e.g.,
-Freshdesk.config "0Fgk8tg9MIHloDV0Kia1",'https://test8653.freshdesk.com'
+Freshdesk.config "rDk2JZ9i5HvQbGu2GX36",'https://test8653.freshdesk.com'
